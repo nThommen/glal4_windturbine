a39fefd5363fdc0f437ea7f7cf2a78c8f5442b80
Nicolas Thommen <nicolas.thommen@pm.me>
2025-06-21 13:18:46 +0200

    slight changes

--- glaL4_windturbine_calcs.py
+++ glaL4_windturbine_calcs.py
@@ -13,6 +13,9 @@ roation_speed = 2 * np.pi * 50
 ang_velocity = 9.1
 gear_box_multiplier = roation_speed / ang_velocity
 gear_box_factor_simscape = 1.0 / gear_box_multiplier
+t_period = 0.02
+frequency = 1 / t_period
+print("Frequency in Hz:", frequency)
 
 print("Rotation speed in rad/s:", roation_speed)
 print("gear_box_factor_simscape:", gear_box_factor_simscape)
